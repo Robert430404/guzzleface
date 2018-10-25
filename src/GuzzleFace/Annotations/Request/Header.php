1a739ddb9aebebf49cdb76586c3a734e52759019
Robert Joseph Cox Jr <robert@studiorclv.com>
2018-10-24 23:51:55 -0700

    Staged additional annotations required for complete guzzle support.

---
+++ src/GuzzleFace/Annotations/Request/Header.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Robert430404\GuzzleFace\Annotations\Request;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * Class Header
+ *
+ * @Annotation
+ * @Annotation\Target({"METHOD"})
+ *
+ * @package Robert430404\GuzzleFace\Annotations\Request
+ */
+class Header extends Annotation
+{
+
+}
