59cdfa8b221c7678aed795ab41e62516b18a6a93
Robert Joseph Cox Jr <rcox@williamhill.us>
2018-11-20 16:47:18 -0800

    Added Enumeration Files

---
+++ src/GuzzleFace/Enumerations/BodyEnumerations.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Robert430404\GuzzleFace\Enumerations;
+
+/**
+ * Class BodyEnumerations
+ *
+ * @package Robert430404\GuzzleFace\Enumerations
+ */
+final class BodyEnumerations
+{
+    /**
+     * These are the available body types.
+     *
+     * @const string
+     */
+    const JSON_BODY = 'JSON';
+    const FORM_PARAMS_BODY = 'FORM_PARAMS';
+    const MULTI_PART_BODY = 'MULTI_PART';
+}
---
+++ src/GuzzleFace/Enumerations/MethodEnumerations.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Robert430404\GuzzleFace\Enumerations;
+
+/**
+ * Class MethodEnumerations
+ *
+ * @package Robert430404\GuzzleFace\Enumerations
+ */
+final class MethodEnumerations
+{
+    /**
+     * These are the allowed HTTP methods for the annotations.
+     *
+     * @const string
+     */
+    const HTTP_POST = 'POST';
+    const HTTP_GET = 'GET';
+    const HTTP_DELETE = 'DELETE';
+    const HTTP_PUT = 'PUT';
+    const HTTP_PATCH = 'PATCH';
+    const HTTP_OPTIONS = 'OPTIONS';
+}
